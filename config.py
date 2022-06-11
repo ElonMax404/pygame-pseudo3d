fa3f155ae9332ac643c800acd2a2e9bd2a1d8702
ElonMax404 <ironhacker@protonmail.com>
2022-06-11 23:45:46 +0200

    Added player class

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ WIDTH = 1200
 HEIGHT = 800
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
+
+player_pos = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+player_speed = 2
+FPS = 60
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from config import *
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+player = Player()
 
 
 def main():
@@ -12,7 +14,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        player.movement()
         screen.fill(pygame.Color("Black"))
+
+        pygame.draw.circle(screen, pygame.Color("Green"), player.pos, 12)
+
+        pygame.display.update()
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
